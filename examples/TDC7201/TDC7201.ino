418331906717493b3d6f6e500db6cccd2f9fc281
Ifediora E C <ifedioraelochukwuc@gmail.com>
2023-05-08 23:51:56 +0200

    Update TDC7201.ino

--- examples/TDC7201/TDC7201.ino
+++ examples/TDC7201/TDC7201.ino
@@ -61,7 +61,7 @@
 
 // Generate an External Clock (Emulate crystal osc.) for the TDC with LEDC Peripheral
 #define LEDC_TIMER                 LEDC_TIMER_0
-#define LEDC_MODE                  LEDC_HIGH_SPEED_MODE
+#define LEDC_MODE                  LEDC_LOW_SPEED_MODE
 #define LEDC_OUTPUT_IO             (25)                                // Define the output GPIO
 #define LEDC_CHANNEL               LEDC_CHANNEL_0
 #define LEDC_DUTY_RES              LEDC_TIMER_5_BIT                    // Set duty resolution to 5 bits
